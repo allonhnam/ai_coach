b1e2e905dda53ac4136ae733b0f0e0408360157d
Allon Nam <allon.nam96@gmail.com>
2025-11-23 18:26:28 -0500

    home page pt1

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,43 @@
-import { Button } from '@/components/ui/button'
+import CTA from '@/components/CTA'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my app</h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+    <main>
+        <h1>Popular Companions</h1>
+
+        <section className='home-section'>
+          <CompanionCard 
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neutal Network of the Brain"
+            subject= "Science"
+            duration={45}
+            color="#ffda6e" 
+          />
+          <CompanionCard 
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject= "Math"
+            duration={30}
+            color="#e5d0ff" 
+          />
+          <CompanionCard 
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="language"
+            subject= "English Literature"
+            duration={30}
+            color="#BDE7FF" 
+          />
+        </section>
+        <section className='home-section'>
+          <CompanionsList />
+          <CTA />
+        </section>
+    </main>
   )
 }
 
