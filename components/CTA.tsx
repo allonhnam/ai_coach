bfced1ff180a589179b7a5ec8d09b61eff8609fb
Allon Nam <allon.nam96@gmail.com>
2025-11-23 20:03:00 -0500

    pt2

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className='cta-section'>
+      <div className='cta-badge'>Start learning your way.</div>
+      <h2>
+        Build and Personalize Learning Companion
+      </h2>
+
+    </section>
   )
 }
 
